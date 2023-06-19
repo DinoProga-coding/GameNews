527f283856f07fbeacd16e25e7c7a8f4b8aa91ad
DinoProga-coding <134218379+DinoProga-coding@users.noreply.github.com>
2023-06-19 21:05:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function onClickButton1(el){
+    window.location = "News.html";
+}
+function onClickButton2(el){
+    window.location = "News2.html";
+}
+
+function onClickButton3(el){
+    window.location = "News3.html";
+}
+function onClickButton4(el){
+    window.location = "News4.html";
+}
+function onClickButton5(el){
+    window.location = "News5.html";
+}
+function onClickButton6(el){
+    window.location = "News5.html";
+}
+function onClickButton7(el){
+    window.location = "News5.html";
+}
